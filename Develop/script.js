aae013af2e7d3d94bce47509fc0806696912060f
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-10-06 13:00:41 -0400

    built out index.html with bootstrap skeleton components

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+$(".saveBtn").on("click", function () {
+  var time = $(this).parent().attr("id");
+  var text = $(this).siblings("textarea").val();
+  localStorage.setItem(time, text);
+});
+
+$("#9 textarea").val(localStorage.getItem("9"));
+$("#10 textarea").val(localStorage.getItem("10"));
+$("#11 textarea").val(localStorage.getItem("11"));
+$("#12 textarea").val(localStorage.getItem("12"));
+$("#13 textarea").val(localStorage.getItem("13"));
+$("#14 textarea").val(localStorage.getItem("14"));
+$("#15 textarea").val(localStorage.getItem("15"));
+$("#16 textarea").val(localStorage.getItem("16"));
+$("#17textarea").val(localStorage.getItem("17"));
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # work-day-scheduler
+
+<!-- time column of 1 -->
+<!-- text area column of 10 -->
+<!-- save column of 1 -->
