d251f5f91d2b17f16bcc2686c5d65ace45d1da9e
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-10-06 21:56:49 -0400

    began working on if statements in js file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// declaring global variables
 var headerDate = document.getElementById("currentDay");
 headerDate.textContent = moment().format("dddd, MMMM Do");
 
@@ -5,12 +6,14 @@ headerDate.textContent = moment().format("dddd, MMMM Do");
 // localStorage.setItem("11", "");
 // localStorage.setItem("17", "");
 
+// save button function
 $(".saveBtn").on("click", function () {
   var time = $(this).parent().attr("id");
   var text = $(this).siblings("textarea").val();
   localStorage.setItem(time, text);
 });
 
+//
 $("#9 textarea").val(localStorage.getItem("9"));
 $("#10 textarea").val(localStorage.getItem("10"));
 $("#11 textarea").val(localStorage.getItem("11"));
@@ -20,3 +23,13 @@ $("#14 textarea").val(localStorage.getItem("14"));
 $("#15 textarea").val(localStorage.getItem("15"));
 $("#16 textarea").val(localStorage.getItem("16"));
 $("#17 textarea").val(localStorage.getItem("17"));
+
+for (i = 0; i < 9; i++) {
+  var calendar = i;
+
+  if (calendarHour < currentHour)
+    if ((calendarHour = currentHour))
+      if (calendarHour > currentHour) console.log(moment().format("h"));
+  console.log(moment().hour());
+  console.log(moment().hours());
+}
