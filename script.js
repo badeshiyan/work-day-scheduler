8298621250a3c11ff4b53ab3dd6fb1d66231df8f
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-10-07 15:20:48 -0400

    cleaned up comments in script.js file

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var headerDate = document.getElementById("currentDay");
 headerDate.textContent = moment().format("dddd, MMMM Do");
 
-// localStorage.setItem("10", "");
-// localStorage.setItem("11", "");
-// localStorage.setItem("17", "");
-
 $(".saveBtn").on("click", function () {
   var time = $(this).parent().attr("id");
   var text = $(this).siblings("textarea").val();
@@ -21,38 +17,33 @@ $("#15 textarea").val(localStorage.getItem("15"));
 $("#16 textarea").val(localStorage.getItem("16"));
 $("#17 textarea").val(localStorage.getItem("17"));
 
-// new code to fix calendar colors
-
 // var currentHour = use moment.js to get the hour of the current time
 var d = new Date();
 var currentHour = d.getHours();
 
 // create a for loop, to iterate through your calendar time blocks
 for (var i = 9; i < 18; i++) {
-  var calendarHour = i; // sets the current position of calendar
-  // time block (for your web application)
+  var calendarHour = i; // sets the current position of calendar time block (for your web application)
+  var divElement = 0;
+  var nodes = 0;
 
   // check to see if calendarHour is in the past, present, or future
-  // if(calendarHour < currentHour -> in the past, color me gray
+  // if calendarHour < currentHour -> in the past, color me gray
   if (calendarHour < currentHour) {
-    var divElement = document.getElementById(calendarHour);
-    var nodes = divElement.childNodes;
-    nodes[3].style.backgroundColor = "gray";
-  }
-  // if calendarHour = currentHour -> in the future, color me red
-  if (calendarHour === currentHour) {
-    var divElement = document.getElementById(calendarHour);
-    var nodes = divElement.childNodes;
-    nodes[3].style.backgroundColor = "red";
+    divElement = document.getElementById(calendarHour);
+    nodes = divElement.children;
+    nodes[1].style.backgroundColor = "#d3d3d3";
   }
   // if calendarHour > currentHour -> in the future, color me green
-  if (calendarHour > currentHour) {
-    var divElement = document.getElementById(calendarHour);
-    var nodes = divElement.childNodes;
-    nodes[3].style.backgroundColor = "green";
+  else if (calendarHour > currentHour) {
+    divElement = document.getElementById(calendarHour);
+    nodes = divElement.children;
+    nodes[1].style.backgroundColor = "#77dd77";
+  }
+  // if calendarHour = currentHour -> in the future, color me red
+  else {
+    divElement = document.getElementById(calendarHour);
+    nodes = divElement.children;
+    nodes[1].style.backgroundColor = "#ff6961";
   }
 }
-
-// console.log(moment().format('h')); // display hour in standard time
-// console.log(moment().hour()); // display hour in military time
-// console.log(moment().hours()); // display hour in military time also
